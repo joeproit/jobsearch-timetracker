d363edd66616df70127d1bfe8c1a804ecdf6d103
JoePro <git@joeproit.com>
2023-05-12 09:40:53 -0500

    enabled bootstrap.min

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
 import TrackerList from "./components/tracker-list.component";
